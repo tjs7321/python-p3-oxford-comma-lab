a7d561163e4647e71a975d41e49a83b5f7e67cd6
Theodore Smith <tjs7321@gmail.com>
2023-08-19 23:41:38 -0400

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) < 2:
+        new_list = ''.join(items)
+    elif len(items) == 2:
+        new_list = ' and '.join(items)
+    elif len(items) > 2:
+        new_list = ', and '.join([', '.join(items[:-1]), items[-1]])
+    return new_list
